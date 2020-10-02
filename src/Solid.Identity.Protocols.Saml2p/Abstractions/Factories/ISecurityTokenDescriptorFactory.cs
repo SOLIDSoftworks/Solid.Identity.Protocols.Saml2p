d1052500615ddbdccd381302d32801c435b92a21
Gísli Konráð Björnsson <gisli.konrad@gmail.com>
2020-10-02 16:28:38 +0000

    cleanup

--- src/Solid.Identity.Protocols.Saml2p/Abstractions/Factories/ISecurityTokenDescriptorFactory.cs
+++ src/Solid.Identity.Protocols.Saml2p/Abstractions/Factories/ISecurityTokenDescriptorFactory.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.IdentityModel.Tokens;
-using Solid.Identity.Protocols.Saml2p.Abstractions.Configuration;
 using Solid.Identity.Protocols.Saml2p.Options;
 using Solid.Identity.Protocols.Saml2p.Models;
 using System;
--- src/Solid.Identity.Protocols.Saml2p/Factories/SecurityTokenDescriptorFactory.cs
+++ src/Solid.Identity.Protocols.Saml2p/Factories/SecurityTokenDescriptorFactory.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
-using Solid.Identity.Protocols.Saml2p.Abstractions.Configuration;
 using Solid.Identity.Protocols.Saml2p.Abstractions.Factories;
 using Solid.Identity.Protocols.Saml2p.Options;
 using Solid.Identity.Protocols.Saml2p.Models;
