c62c2a32daef8002f443d2c2d7554aefeb21b084
Gísli Konráð Björnsson <gisli.konrad@gmail.com>
2020-10-22 11:39:49 +0000

    Adds some xml comments

--- src/Solid.Identity.Protocols.Saml2p/Abstractions/ISaml2pIdentityProvider.cs
+++ src/Solid.Identity.Protocols.Saml2p/Abstractions/ISaml2pIdentityProvider.cs
@@ -9,11 +9,29 @@ using System.Threading.Tasks;
 
 namespace Solid.Identity.Protocols.Saml2p.Abstractions
 {
+    /// <summary>
+    /// An interface describing an IDP partner. 
+    /// </summary>
     public interface ISaml2pIdentityProvider : ISaml2pPartner<Saml2pServiceProviderEvents>
     {
+        /// <summary>
+        /// The subject name id format requested from the IDP partner.
+        /// </summary>
         string NameIdPolicyFormat { get; }
+
+        /// <summary>
+        /// The requested authentication type from the IDP partner.
+        /// </summary>
         Uri RequestedAuthnContextClassRef { get; }
+
+        /// <summary>
+        /// The endpoint on the IDP partner service that is used when starting SSO.
+        /// </summary>
         PathString AcceptSsoEndpoint { get; }
+
+        /// <summary>
+        /// A collection of <see cref="SecurityKey"/>s that are used to validate the SAML2 security token coming from the IDP partner.
+        /// </summary>
         ICollection<SecurityKey> AssertionSigningKeys { get; }
     }
 }
--- src/Solid.Identity.Protocols.Saml2p/Abstractions/ISaml2pPartner.cs
+++ src/Solid.Identity.Protocols.Saml2p/Abstractions/ISaml2pPartner.cs
@@ -1,19 +1,55 @@
 ﻿using Solid.Identity.Protocols.Saml2p.Models;
+using Solid.Identity.Protocols.Saml2p.Options;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Solid.Identity.Protocols.Saml2p.Abstractions
 {
+    /// <summary>
+    /// An interface describing the base of a SAML2P SSO partner.
+    /// </summary>
+    /// <typeparam name="TEvents">A type containing events to be run.</typeparam>
     public interface ISaml2pPartner<TEvents>
     {
+        /// <summary>
+        /// The id of the partner. 
+        /// </summary>
         string Id { get; }
+
+        /// <summary>
+        /// The issuer id that the partner expects. If it is null, <see cref="Saml2pOptions.Issuer"/> is used.
+        /// </summary>
         string ExpectedIssuer { get; }
+
+        /// <summary>
+        /// The name of the partner.
+        /// </summary>
         string Name { get; }
+
+        /// <summary>
+        /// The base url for the parnter service.
+        /// </summary>
         Uri BaseUrl { get; }
+
+        /// <summary>
+        /// A flag tindicating whether the partner is enabled or not.
+        /// </summary>
         bool Enabled { get; }
+
+        /// <summary>
+        /// A flag indicating whether the partner can initiate SSO.
+        /// </summary>
         bool CanInitiateSso { get; }
+
+        /// <summary>
+        /// The bindings supported by the partner.
+        /// </summary>
         ICollection<BindingType> SupportedBindings { get; }
+
+        /// <summary>
+        /// The events object.
+        /// </summary>
         TEvents Events { get; }
     }
 }
