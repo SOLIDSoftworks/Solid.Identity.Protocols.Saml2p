174fb1254098671e02883135bb7114e7497bb2e6
Gísli Konráð Björnsson <gisli.konrad@gmail.com>
2024-11-05 15:50:19 +0000

    Adds logout button to samples

---
+++ src/AspNetCore.IdpSample/Pages/Logout.cshtml.cs
@@ -0,0 +1,15 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AspNetCore.IdpSample.Pages;
+
+public class Logout : PageModel
+{
+    public async Task<IActionResult> OnGetAsync()
+    {
+        await HttpContext.SignOutAsync();
+        return Redirect("/");
+    }
+}
